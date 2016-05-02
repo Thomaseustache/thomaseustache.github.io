d5f2f2d072ec5af95a4f784f2cb9939b34fd7bf0
Thomas Eustache <thomas.eustache@being.fr>
2016-05-02 11:03:51 +0200

    php

---
+++ ac02/site/1440-no-cover/send_mail.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+if(isset($_POST['submit'])){
+	
+    $to = "eustacht@gmail.com"; // this is your Email address
+    $from = $_POST['mail']; // this is the sender's Email address
+    $first_name = $_POST['nom'];
+    $last_name = $_POST['prenom'];
+	
+    foreach ($_POST as $key => $value) {
+		$message .= $key ."=>". $value;
+	}
+	
+    // $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+	
+}
+	
+	
+?>
